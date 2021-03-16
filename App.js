1f1c84e1af3e0e8715d103d44baa6f482fa97c70
Jaquan1314 <javaughngrant@gmail.com>
2021-03-16 19:03:22 -0400

    NavigationContainer added to pass navigation props to its children

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
 import LandingStack from './src/routes/LandingStack';
 import store from './src/redux/store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <LandingStack />
+      <NavigationContainer>
+        <LandingStack />
+      </NavigationContainer>
     </Provider>
   );
 }
