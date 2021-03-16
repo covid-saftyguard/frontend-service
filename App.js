db0abdde633e4eaf6dbb9e391b584a32cfff6187
techtwins <gshevaughn@gmail.com>
2021-03-16 18:00:36 -0400

    setting up the structure for the homepage

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import LandingStack from "./src/routes/LandingStack";
+import Home from "./src/screens/Home";
 import store from "./src/redux/store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <LandingStack />
+      <Home />
+      {/* <LandingStack /> */}
     </Provider>
   );
 }
