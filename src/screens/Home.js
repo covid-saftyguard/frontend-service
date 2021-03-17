d7a6befb146f1b13b1a669c3a94ee87520a303f6
techtwins <gshevaughn@gmail.com>
2021-03-16 23:37:07 -0400

    display available vaccines and symptoms

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <>
+    <ScrollView>
       <View style={{ justifyContent: "space-around", flexDirection: "row" }}>
         <View style={styles.container}>
           <Text style={styles.h3}>Cases</Text>
@@ -31,16 +31,126 @@ export default function Home() {
           width: 400,
         }}
       >
-        <Text>This will be a line chart</Text>
+        <Text>Line chart here once data is received</Text>
       </View>
-    </>
+      <View
+        style={{
+          ...styles.container,
+          marginLeft: 15,
+          marginTop: 20,
+          backgroundColor: "#73DCE9",
+          height: 300,
+          width: 400,
+        }}
+      >
+        <Text
+          style={{
+            marginTop: 10,
+            marginBottom: 20,
+            fontSize: 22,
+            fontWeight: "600",
+          }}
+        >
+          Vaccines Available
+        </Text>
+        {/* once api data is received, map through to create each view component below */}
+        <View
+          style={{
+            backgroundColor: "#548CD6",
+            padding: 20,
+            width: "100%",
+            borderRadius: 20,
+            marginBottom: 10,
+          }}
+        >
+          <Text style={{ ...styles.h3, color: "#fff", fontWeight: "500" }}>
+            Pfizer Vaccine
+          </Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#548CD6",
+            padding: 20,
+            width: "100%",
+            borderRadius: 20,
+            marginBottom: 10,
+          }}
+        >
+          <Text style={{ ...styles.h3, color: "#fff", fontWeight: "500" }}>
+            Moderna Vaccine
+          </Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#548CD6",
+            padding: 20,
+            width: "100%",
+            borderRadius: 20,
+            marginBottom: 10,
+          }}
+        >
+          <Text style={{ ...styles.h3, color: "#fff", fontWeight: "500" }}>
+            Johnson & Johnson Vaccine
+          </Text>
+        </View>
+      </View>
+      <View style={{ marginLeft: 15, marginTop: 25, marginBottom: 15 }}>
+        <Text style={{ ...styles.h1, fontSize: 30 }}>Symptoms</Text>
+      </View>
+      <ScrollView horizontal="true">
+        <View
+          style={{
+            padding: 40,
+            backgroundColor: "#F8F8F8",
+            width: 300,
+            borderRadius: 20,
+            marginLeft: 15,
+          }}
+        >
+          <Image />
+          <Text
+            style={{
+              color: "#548CD6",
+              fontWeight: "800",
+              fontSize: 18,
+              marginBottom: 10,
+            }}
+          >
+            High Fever
+          </Text>
+          <Text>Lorem ipsum lorem ipsum</Text>
+        </View>
+        <View
+          style={{
+            padding: 40,
+            backgroundColor: "#F8F8F8",
+            width: 300,
+            borderRadius: 20,
+            marginLeft: 15,
+          }}
+        >
+          <Image />
+          <Text
+            style={{
+              color: "#548CD6",
+              fontWeight: "800",
+              fontSize: 18,
+              marginBottom: 10,
+            }}
+          >
+            Dry Cough
+          </Text>
+          <Text>Lorem ipsum lorem ipsum</Text>
+        </View>
+      </ScrollView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    borderColor: "black",
-    borderWidth: 1.5,
+    // borderColor: "black",
+    // borderWidth: 1.5,
     marginTop: 120,
     width: 180,
     height: 130,
